1ce93a770a50d95dd26d637db235834ff0760ead
gatkin <greg.scott.atkin@gmail.com>
2017-02-21 19:42:36 -0600

    Create definitions for message initialization functions

---
+++ src/main/scala/codegen/types/InitFunctionDefinition.scala
@@ -0,0 +1,73 @@
+package codegen.types
+
+import codegen.functions._
+import datamodel._
+
+
+object InitFunctionDefinition {
+
+  private val parameterName = "obj"
+
+  /**
+    * Creates a definition for a function that can be
+    * used to initialize a message struct. The initialization
+    * function will simply zero out the message struct
+    * @param message cDTO message
+    * @return Definition for the message's initialization function
+    */
+  def apply(message: Message): FunctionDefinition = {
+    FunctionDefinition(
+      name = name(message),
+      documentation = documentation(message),
+      prototype = prototype(message),
+      body = body(message)
+    )
+  }
+
+  /**
+    * Gets the name of the message's initialization function
+    * @param message cDTO message
+    * @return Name of the initialization function
+    */
+  private def name(message: Message): String = {
+    s"${message.name}_init"
+  }
+
+  /**
+    * Gets the documentation for the message's initialization function
+    * @param message cDTO message
+    * @return Documentation for the initialization function
+    */
+  private def documentation(message: Message): FunctionDocumentation = {
+    FunctionDocumentation(
+      shortSummary = s"Initialize a ${message.name} object",
+      description = s"Zeros out the provided ${message.name}."
+    )
+  }
+
+  /**
+    * Gets the body of the message's initialization function
+    * @param message cDTO message
+    * @return String containing the initialization function's body
+    */
+  private def body(message: Message): String = {
+    s"memset( $parameterName, 0, sizeof( *$parameterName ) );"
+  }
+
+  /**
+    * Gets the prototype for the message's initialization function.
+    * Message initialization functions are never static and are
+    * declared in the same header that defines the message struct.
+    * @param message cDTO message
+    * @return Prototype of the initialization function
+    */
+  private def prototype(message: Message): FunctionPrototype = {
+    FunctionPrototype(
+      isStatic = false,
+      returnType = "void",
+      parameters = List(
+        FunctionParameter(paramType = s"${message.name}*", paramName = parameterName)
+      )
+    )
+  }
+}
---
+++ src/test/scala/codegen/types/InitFunctionDefinitionSpec.scala
@@ -0,0 +1,29 @@
+package codegen.types
+
+import datamodel._
+import dto.UnitSpec
+import codegen.functions._
+
+class InitFunctionDefinitionSpec extends UnitSpec {
+
+  "Init function definition" should "create an initialization function for a cDTO message" in {
+
+    val message = Message("issue", List(
+      Field("number", AliasedType("uint32_t", NumberType), "issueNumber"),
+      Field("url", DynamicStringType, "url"),
+      Field("creator", ObjectType("user"), "user")
+    ))
+
+    val expectedDefinition = FunctionDefinition(
+      name = "issue_init",
+      documentation = FunctionDocumentation(shortSummary = "Initialize a issue object", description = "Zeros out the provided issue."),
+      prototype = FunctionPrototype(isStatic = false, returnType = "void", parameters = List(
+        FunctionParameter(paramType = "issue*", paramName = "obj")
+      )),
+      body = "memset( obj, 0, sizeof( *obj ) );"
+    )
+
+    InitFunctionDefinition(message) shouldBe expectedDefinition
+  }
+
+}
